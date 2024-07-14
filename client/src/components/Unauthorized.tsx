318a3ab32577281ffad3ff690578a1bb87b8f539
avivisovitz1 <162724346+avivisovitz1@users.noreply.github.com>
2024-07-14 14:13:32 -0700

    change forms app logo to be the same in login & signUp & PQ (#11)
    * change forms app logo to be the same in login & signUp & PQ
    
    * adjust UI of the unauthorized page

--- client/src/components/Unauthorized.tsx
+++ client/src/components/Unauthorized.tsx
@@ -1,22 +1,36 @@
-import { HOME_URL } from "@/router/router.const"
-import { Button } from "@mui/material"
-import React from "react"
-import { useNavigate } from "react-router-dom"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HOME_URL } from '@/router/router.const';
+import { Button, Grid, Typography } from '@mui/material';
 
 const Unauthorized: React.FC = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <div>
-      <h1 style={{ color: "red" }}>Unauthorized</h1>
-      <p style={{ color: "white" }}>
-        You are not authorized to access this page.
-      </p>
-      <Button color="error" onClick={() => navigate(HOME_URL)}>
-        Go Home
+    <Grid
+      container
+      direction={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      height={'100%'}
+    >
+      <Typography variant={'h1'} color='error' fontWeight={600}>
+        {'Unauthorized'}
+      </Typography>
+      <Typography variant={'h4'} color={'primary.light'}>
+        {'You are not authorized to access this page.'}
+      </Typography>
+      <Button
+        color={'error'}
+        sx={{ textDecoration: 'underline' }}
+        onClick={() => navigate(HOME_URL)}
+      >
+        <Typography variant={'h4'} color={'error'} fontWeight={600}>
+          {'Go Home'}
+        </Typography>
       </Button>
-    </div>
-  )
-}
+    </Grid>
+  );
+};
 
-export default Unauthorized
+export default Unauthorized;
