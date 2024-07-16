d01fb6ae30567f41da4f818690ce56fb635d26ba
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-07-16 21:03:19 +0300

    add cors headers

---
+++ server/middlewares/cors_middleware.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const cors = (req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
