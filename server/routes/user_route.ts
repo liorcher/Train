4cbb270094a394060646aeb014fcf4fedd979a48
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-08-02 16:16:23 +0300

    add missing file

--- server/routes/user_route.ts
+++ server/routes/user_route.ts
@@ -1,15 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import { checkAuth } from '../middlewares/auth_middleware';
-import { pool } from '../dal/data_access';
+import { UserController } from '../controllers';
 
 export const userRouter = express.Router();
 
-userRouter.get('/', checkAuth, (req: Request, res: Response, next: NextFunction) => {
-    pool.query('SELECT * FROM "trAIn".users ORDER BY user_id ASC', (error, results) => {
-        if (error) {
-            console.log(error);
-            throw error;
-        }
-        res.status(200).json(results.rows);
-    });
-});
+userRouter.get('/me', checkAuth, UserController.getCurrentUser);
