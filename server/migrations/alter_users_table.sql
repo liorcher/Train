3716ecea6a34fcad491abbbb54fb26529b47bcbd
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-08-03 13:50:22 +0300

    add mig file

--- server/migrations/alter_users_table.sql
+++ server/migrations/alter_users_table.sql
@@ -16,3 +16,17 @@ ALTER TABLE IF EXISTS "trAIn".users
     DROP COLUMN first_name,
     DROP COLUMN last_name,
     ADD COLUMN name text;
+
+ALTER TABLE "trAIn".users
+    ALTER COLUMN gender TYPE text;
+ALTER TABLE IF EXISTS "trAIn".users DROP CONSTRAINT IF EXISTS users_activity_level_check;
+
+ALTER TABLE IF EXISTS "trAIn".users
+    ADD CONSTRAINT users_activity_level_check CHECK (activity_level::text = ANY (ARRAY['Beginner'::character varying, 'Intermediate'::character varying, 'Advanced'::character varying]::text[]))
+    NOT VALID;
+
+ALTER TABLE IF EXISTS "trAIn".users DROP CONSTRAINT IF EXISTS users_gender_check;
+
+ALTER TABLE IF EXISTS "trAIn".users
+    ADD CONSTRAINT users_gender_check CHECK (gender::text = ANY (ARRAY['Male'::character varying::text, 'Female'::character varying::text]))
+    NOT VALID;
