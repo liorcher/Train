b24cef458d72918a3d5c354e5ab9831d67cbdd61
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-08-04 21:49:02 +0300

    add calendar view option to workouts page

--- client/src/components/Workouts/WorkoutPlan/WorkoutPlan.tsx
+++ client/src/components/Workouts/WorkoutPlan/WorkoutPlan.tsx
@@ -1,51 +1,70 @@
-import Loader from "@/components/Loader"
-import { Workout } from "@/types/workout.type"
-import { Refresh } from "@mui/icons-material"
-import { Box, IconButton, Tooltip, Typography } from "@mui/material"
-import { Dispatch, SetStateAction } from "react"
-import Styles from "./WorkoutPlan.style"
-import WorkoutPlanItem from "./WorkoutPlanItem"
+import Loader from '@/components/Loader';
+import { Workout } from '@/types/workout.type';
+import { CalendarToday, FormatListBulleted, Refresh } from '@mui/icons-material';
+import { Box, Grid, IconButton, Tooltip, Typography } from '@mui/material';
+import { Dispatch, SetStateAction, useState } from 'react';
+import Styles from './WorkoutPlan.style';
+import WorkoutPlanItem from './WorkoutPlanItem';
+import { WorkoutsCalendar } from '../WorkoutsCalendar';
 
 type Props = {
-  workouts: Workout[] | null
-  setWorkout: Dispatch<SetStateAction<Workout | null>>
-  fetchWorkouts: () => void
-}
+  workouts: Workout[] | null;
+  setWorkout: Dispatch<SetStateAction<Workout | null>>;
+  fetchWorkouts: () => void;
+};
 
 const WorkoutPlan = ({ workouts, setWorkout, fetchWorkouts }: Props) => {
+  const [isRegularView, setIsRegularView] = useState(true);
+
   return (
     <Box sx={Styles.outerBox}>
-      <Box sx={Styles.titleBox}>
-        <Typography {...Styles.title}>Your Weekly Workout Plan</Typography>
-        <Box sx={Styles.refreshIconBox}>
-          <Tooltip title="Refresh workout plan">
-            <IconButton onClick={fetchWorkouts}>
-              <Refresh {...Styles.refreshIcon} />
-            </IconButton>
-          </Tooltip>
-        </Box>
-      </Box>
-
-      {!workouts ? (
-        <Loader />
-      ) : workouts.length == 0 ? (
-        <Typography {...Styles.workoutPlanItemField}>
-          Looks like no training this week...
-        </Typography>
+      <Grid item alignSelf={'flex-start'}>
+        <Tooltip title={isRegularView ? 'Calendar View' : 'List View'}>
+          <IconButton onClick={() => setIsRegularView((prev) => !prev)}>
+            {!isRegularView ? (
+              <FormatListBulleted sx={{ color: 'primary.light' }} />
+            ) : (
+              <CalendarToday sx={{ color: 'primary.light' }} />
+            )}
+          </IconButton>
+        </Tooltip>
+      </Grid>
+      {isRegularView ? (
+        <>
+          <Box sx={Styles.titleBox}>
+            <Typography {...Styles.title}>Your Weekly Workout Plan</Typography>
+            <Box sx={Styles.refreshIconBox}>
+              <Tooltip title='Refresh workout plan'>
+                <IconButton onClick={fetchWorkouts}>
+                  <Refresh {...Styles.refreshIcon} />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Box>
+          {!workouts ? (
+            <Loader />
+          ) : workouts.length == 0 ? (
+            <Typography {...Styles.workoutPlanItemField}>
+              Looks like no training this week...
+            </Typography>
+          ) : (
+            <Box sx={Styles.workoutList}>
+              {workouts.map((workout, index) => (
+                <WorkoutPlanItem
+                  key={index}
+                  workout={workout}
+                  setWorkout={setWorkout}
+                  index={index}
+                />
+              ))}
+            </Box>
+          )}
+        </>
       ) : (
-        <Box sx={Styles.workoutList}>
-          {workouts.map((workout, index) => (
-            <WorkoutPlanItem
-              key={index}
-              workout={workout}
-              setWorkout={setWorkout}
-              index={index}
-            />
-          ))}
-        </Box>
+        <WorkoutsCalendar workouts={workouts} setWorkout={setWorkout} />
       )}
     </Box>
-  )
-}
+  );
+};
 
-export default WorkoutPlan
+export default WorkoutPlan;
--- client/src/components/Workouts/WorkoutsPage.tsx
+++ client/src/components/Workouts/WorkoutsPage.tsx
@@ -1,60 +1,54 @@
-import { Workout } from "@/types/workout.type"
-import { Box, Stack } from "@mui/material"
-import { find, map } from "lodash"
-import React, { useEffect, useState } from "react"
-import WorkoutActivity from "./WorkoutActivity/WorkoutActivity"
-import WorkoutPlan from "./WorkoutPlan/WorkoutPlan"
-import Styles from "./WorkoutsPage.style"
-import { workoutsMockData } from "./workoutsMockData"
+import { Workout } from '@/types/workout.type';
+import { Box, Stack } from '@mui/material';
+import { find, map } from 'lodash';
+import React, { useEffect, useState } from 'react';
+import WorkoutActivity from './WorkoutActivity/WorkoutActivity';
+import WorkoutPlan from './WorkoutPlan/WorkoutPlan';
+import Styles from './WorkoutsPage.style';
+import { workoutsMockData } from './workoutsMockData';
 
 const WorkoutsPage: React.FC = () => {
-  const [workouts, setWorkouts] = useState<Workout[] | null>(null)
-  const [workout, setWorkout] = useState<Workout | null>(null)
+  const [workouts, setWorkouts] = useState<Workout[] | null>(null);
+  const [workout, setWorkout] = useState<Workout | null>(null);
 
   useEffect(() => {
-    fetchWorkouts()
-  }, [])
+    fetchWorkouts();
+  }, []);
 
   const fetchWorkouts = () => {
     // const serverWorkouts = api.get('/workouts')
-    const serverWorkouts = workoutsMockData
+    const serverWorkouts = workoutsMockData;
 
-    setWorkouts(null)
+    setWorkouts(null);
     setTimeout(() => {
-      setWorkouts(serverWorkouts)
+      setWorkouts(serverWorkouts);
       const updatedWorkout =
-        find(serverWorkouts, { id: workout?.id }) ||
-        (serverWorkouts && serverWorkouts[0]) ||
-        null
-      setWorkout(updatedWorkout)
-    }, 1000)
-  }
+        find(serverWorkouts, { id: workout?.id }) || (serverWorkouts && serverWorkouts[0]) || null;
+      setWorkout(updatedWorkout);
+    }, 1000);
+  };
 
   const updateWorkout = (updatedWorkout: Workout) => {
-    if (!workouts) return
+    if (!workouts) return;
 
     // const updatedWorkouts = api.post('/workout/:id', { workout: updatedWorkout })
     // fetchWorkouts()
 
     const updatedWorkouts = map(workouts, (currentWorkout: Workout) =>
       currentWorkout.id === updatedWorkout.id ? updatedWorkout : currentWorkout
-    )
-    setWorkouts(updatedWorkouts)
-    setWorkout(updatedWorkout)
-  }
+    );
+    setWorkouts(updatedWorkouts);
+    setWorkout(updatedWorkout);
+  };
 
   return (
     <Box sx={Styles.outerBox}>
       <Stack {...Styles.stack}>
-        <WorkoutPlan
-          workouts={workouts}
-          setWorkout={setWorkout}
-          fetchWorkouts={fetchWorkouts}
-        />
+        <WorkoutPlan workouts={workouts} setWorkout={setWorkout} fetchWorkouts={fetchWorkouts} />
         <WorkoutActivity workout={workout} updateWorkout={updateWorkout} />
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default WorkoutsPage
+export default WorkoutsPage;
