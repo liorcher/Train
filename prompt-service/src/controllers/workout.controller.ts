239cd5f5763d687378b90d81e5668031bf5d5960
liorcher <lior.cher007@gmail.com>
2024-08-05 18:38:41 +0300

    adding generic requests to chatgptt

--- prompt-service/src/controllers/workout.controller.ts
+++ prompt-service/src/controllers/workout.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import { getWorkoutPlan } from "../dal/workout.dal";
 
 export const getNewWorkoutPlan = async (req: Request, res: Response) => {
+  const userPreferences = "i want to do only two workouts per week";
+
   try {
-    const newWorkoutPlan = await getWorkoutPlan();
+    const newWorkoutPlan = await getWorkoutPlan(userPreferences);
     res.json(newWorkoutPlan);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
--- prompt-service/src/dal/workout.dal.ts
+++ prompt-service/src/dal/workout.dal.ts
@@ -1,5 +1,39 @@
-import { hardCodedSimpleResponse } from "./queries/workout.queries"
+import {
+  Messages,
+  openai,
+  sendChatGPTQuery,
+} from "../services/chatgpt.service";
+import { workoutPromptIntructions } from "./queries/workout.queries";
+import {
+  Workout,
+  workoutFunctions,
+  workoutFunctionsCalls,
+} from "../models/workout.model";
 
-export const getWorkoutPlan = () =>{
-    return hardCodedSimpleResponse;
-}
+export const getWorkoutPlan = async (userRequests: string) => {
+  try {
+    const messages: Messages = [
+      {
+        role: "system",
+        content: workoutPromptIntructions,
+      },
+      {
+        role: "user",
+        content: userRequests,
+      },
+    ];
+
+    const response = await sendChatGPTQuery({
+      messages,
+      function_call: "auto",
+      functions: workoutFunctions,
+    });
+
+    const functionCall = response.choices[0].message.function_call;
+    const json = <Workout[]>JSON.parse(functionCall?.arguments as string);
+
+    return { json };
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
