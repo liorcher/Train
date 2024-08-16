2f996fa0e8183ab7c8311edc0fe475fc62ad9b23
yaeltzahor <yaeltzahor@gmail.com>
2024-08-16 20:25:28 +0300

    add create workpout plan

--- server/controllers/user_controller.ts
+++ server/controllers/user_controller.ts
@@ -1,7 +1,8 @@
 import type { Response, Request } from 'express';
 import { getUser } from '../dal/users_dal';
+import { getGoalsByUser } from '../dal/goals_dal';
 
-const getCurrentUser = async (req: Request, res: Response) => {
+export const getCurrentUser = async (req: Request, res: Response) => {
     try {
         const result = getUser(req.user.userId);
         if (result) {
@@ -14,4 +15,16 @@ const getCurrentUser = async (req: Request, res: Response) => {
     }
 };
 
-export default { getCurrentUser };
+
+
+export const createUserWorkoutPlan = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user.userId
+        const user = getUser(userId);
+        const goals = getGoalsByUser(userId)
+        const preferences = {...user, ...goals}
+        //TODO: call gpt with preferences
+    } catch (error) {
+        res.status(400).send('An error occurred while create user workout plan');
+    }
+};
--- server/dal/goals_dal.ts
+++ server/dal/goals_dal.ts
@@ -14,5 +14,11 @@ export const saveGoals = async (preferences, userId: string) => {
         'INSERT INTO "trAIn".goals (user_id, target_weight, goals, days, workouts, workout_duration_in_minutes) VALUES ($1, $2, $3, $4, $5, $6)',
         userGoalsParams
     );
+}
 
+export const getGoalsByUser = async (userId: string) => {
+    const result = await query(
+        'SELECT * FROM "trAIn".goals WHERE user_id = $1',
+        [userId]
+    );
 }
