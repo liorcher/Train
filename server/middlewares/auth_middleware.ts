55d7d95de4961460335dbc37bee8c5297ef889da
yaeltzahor <yaeltzahor@gmail.com>
2024-08-18 23:14:27 +0300

    without comments

--- server/middlewares/auth_middleware.ts
+++ server/middlewares/auth_middleware.ts
@@ -3,19 +3,17 @@ import jwt, { type JwtPayload } from 'jsonwebtoken';
 import { getTokenFromRequest } from '../utils/auth_util';
 
 export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
-    // const token = getTokenFromRequest(req);
+    const token = getTokenFromRequest(req);
 
-    // //unauthorized
-    // if (token == null) return res.sendStatus(401);
+    //unauthorized
+    if (token == null) return res.sendStatus(401);
 
-    // jwt.verify(token, process.env.TOKEN_SECRET, (error, data: JwtPayload) => {
-    //     //invalid request
-    //     if (error) return res.sendStatus(403);
+    jwt.verify(token, process.env.TOKEN_SECRET, (error, data: JwtPayload) => {
+        //invalid request
+        if (error) return res.sendStatus(403);
 
-    //     const userId = data.userId;
-    //     req.user = { userId };
-    //     return next();
-    // });
-    req.user = { userId: '881eccaa-e11d-4c87-8256-46ac887df765' };
-    return next();
+        const userId = data.userId;
+        req.user = { userId };
+        return next();
+    });
 };
