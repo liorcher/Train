be4311d5ff4f67c03ebb101d8f06596f4cbefba5
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-08-31 18:30:53 +0300

    create http/s by node env

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,7 @@
 import { initApp } from './app';
+import fs from 'fs';
+import https from 'https';
+import http from 'http';
 
 const port = process.env.PORT || 3000;
 
@@ -6,4 +9,16 @@ initApp().then((app) => {
     app.listen(port, () => {
         console.log('Server Listening on PORT:', port);
     });
+
+    if (process.env.NODE_ENV === 'production') {
+        console.log('server is running in production mode');
+        const certs = {
+            key: fs.readFileSync('./client-key.pem'),
+            cert: fs.readFileSync('./client-cert.pem'),
+        };
+        https.createServer(certs, app).listen(port);
+    } else {
+        console.log('server is running in development mode');
+        http.createServer(app).listen(port);
+    }
 });
