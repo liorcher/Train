e9bbdecc67cae810e6eb4c7ea0a00ed2e081984f
yaeltzahor <yaeltzahor@gmail.com>
2024-08-31 19:46:51 +0300

    change connection to db

--- server/configs/appConfig.ts
+++ server/configs/appConfig.ts
@@ -1,9 +1,9 @@
 export default {
     db: {
-        user: 'avnadmin',
-        host: 'train-2024-yaeltzahor110700-d411.f.aivencloud.com',
+        user: 'postgres',
+        host: process.env.DB_HOST || '10.10.248.143',
         name: process.env.DB_NAME || 'trAIn',
-        port: parseInt(process.env.DB_PROT) || 19080,
+        port: parseInt(process.env.DB_PROT) || 5432,
         password: process.env.PG_PASSWORD,
     },
 };
