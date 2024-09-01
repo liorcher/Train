0aad64c6caae1e595ee3a9fc7dc7ec049af79b4d
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-09-01 19:15:01 +0300

    revert changes locally

--- server/configs/appConfig.ts
+++ server/configs/appConfig.ts
@@ -1,12 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 export default {
     db: {
-        user: 'postgres',
+        user: 'avnadmin',
         host: process.env.DB_HOST || '10.10.248.143',
         name: process.env.DB_NAME || 'trAIn',
-        port: parseInt(process.env.DB_PROT) || 5432,
+        port: parseInt(process.env.DB_PROT) || 19080,
         password: process.env.PG_PASSWORD,
     },
 };
