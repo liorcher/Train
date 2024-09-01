22616166ef6806c2e65e54b16ce169ce3b78b595
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-09-01 19:41:11 +0300

    fix main screen alignment

--- client/src/components/ProgressCharts/ProgressCharts.tsx
+++ client/src/components/ProgressCharts/ProgressCharts.tsx
@@ -1,31 +1,31 @@
-import { Chart } from "@/types/chart.type"
-import { Grid, Typography } from "@mui/material"
-import { Gauge } from "@mui/x-charts/Gauge"
-import Styles from "./ProgressCharts.style"
+import { Chart } from '@/types/chart.type';
+import { Grid, Typography } from '@mui/material';
+import { Gauge } from '@mui/x-charts/Gauge';
+import Styles from './ProgressCharts.style';
 
 const ProgressCharts: React.FC = () => {
-  const mockData = { caloriesBurned: 20, weightLoss: 10 }
+  const mockData = { caloriesBurned: 20, weightLoss: 10 };
   const charts: Chart[] = [
-    { title: "Calories Burn", data: mockData.caloriesBurned },
-    { title: "Weight Loss", data: mockData.weightLoss },
-  ]
+    { title: 'Burnt Calories', data: mockData.caloriesBurned },
+    { title: 'Weight Loss', data: mockData.weightLoss },
+  ];
 
   const gaugeSettings = (chart: Chart) => ({
     value: chart.data,
-    cornerRadius: "50%",
+    cornerRadius: '50%',
     sx: Styles.gauge,
     text: `${chart.data}%`,
-  })
+  });
 
   return (
-    <Grid container direction="row" sx={{ gap: 30, margin: "5vw" }}>
+    <Grid container direction='row' sx={{ gap: 30, margin: '5vw' }}>
       {charts.map((chart, index) => (
         <Grid
           item
           key={index}
           sx={{
-            justifyContent: "center",
-            alignItems: "center",
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
           <Typography sx={Styles.title}>{chart.title}</Typography>
@@ -33,7 +33,7 @@ const ProgressCharts: React.FC = () => {
         </Grid>
       ))}
     </Grid>
-  )
-}
+  );
+};
 
-export default ProgressCharts
+export default ProgressCharts;
